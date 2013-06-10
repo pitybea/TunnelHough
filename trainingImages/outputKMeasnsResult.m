e27576062b1c5f14b6072f5d2e172ffb6773add0
Your Name <you@example.com>
2013-06-11 00:14:20 +0900

    ok

--- trainingImages/outputKMeasnsResult.m
+++ trainingImages/outputKMeasnsResult.m
@@ -1,3 +1,20 @@
 function outputKMeasnsResult()
 load('features.txt');
+i=10;
+while i<121
+    [idx,c]=kmeans(features,i,'emptyaction','drop','MaxIter',10000);
+    fid=fopen(sprintf('kmeancenter%d.txt',i),'w');
+    for j=1:size(c,1)
+        fprintf(fid,'%f ',c(j,:));
+        fprintf(fid,'\n');
+    end
+    fclose(fid);
+    fid=fopen(sprintf('kmeanbelong%d.txt',i),'w');
+    
+    fprintf(fid,'%d\n',idx);
+        
+    fclose(fid);
+    i=i+10;
+    i
+end
 end
